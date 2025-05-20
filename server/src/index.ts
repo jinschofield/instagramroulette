651042cffb7364a4fd3ce9f5c5d3b7a84a62aa7d
myles332 <myles.anderson@princeton.edu>
2025-05-20 01:43:33 -0400

    delete node_modules

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,37 @@
 import express from 'express';
 import { createServer } from 'node:http';
+import { Server } from 'socket.io';
 
 const app = express();
 const server = createServer(app);
+const io = new Server(server);
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
-});
+const userUrlsMap = new Map<string, string[] | null>();
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('join', (username) => {
+    // TODO: make sure can't take someone else's username
+    if (!userUrlsMap.has(username)) {
+      userUrlsMap.set(username, null);
+      io.emit("lobby_update", Object.fromEntries(userUrlsMap));
+    }
+  });
+
+  socket.on('geturls', (username, links) => {
+    if (typeof username !== 'string' || !Array.isArray(links) || links.some(link => typeof link !== 'string')) {
+      console.log('Invalid input. Expecting a username and an array of links.');
+    }
+    userUrlsMap.set(username, links);
+    console.log(`Received links for user: ${username}`);
+    console.log(links);
+    io.emit("lobby_update", Object.fromEntries(userUrlsMap));
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+})
 
 server.listen(3000, () => {
   console.log('server running at http://localhost:3000');
